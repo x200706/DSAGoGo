a1ff94bdd8e7195fc5bd22ae5ceb8d76f42aadff
Crystal <chi200706@gmail.com>
2023-05-08 14:23:13 +0800

    unfinished practise

--- src/main/java/org/example/Array.java
+++ src/main/java/org/example/Array.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 public class Array {
     @Test
-    public void test(){
+    public void onceNumOutput() { //JUnit之測試方法似乎不能有回傳值
+        int[] arr = {3, 1, 2, 1, 2, 3, 5, 7, 7};
+        System.out.println(onceNum(arr));
+    }
 
+    public int onceNum(int[] arr) { //出自力扣熱題100；感覺這題是關魚哈希表
+        return 0;
     }
 }
