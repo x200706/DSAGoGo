69b4d09748634fe2489e564093258140d3f10ef7
Crystal <99391710+x200706@users.noreply.github.com>
2023-05-08 14:50:08 +0800

    Comment fix

--- src/main/java/org/example/Array.java
+++ src/main/java/org/example/Array.java
@@ -9,7 +9,7 @@ public class Array {
         System.out.println(onceNum(arr));
     }
 
-    public int onceNum(int[] arr) { //出自力扣熱題100；感覺這題是關魚哈希表
+    public int onceNum(int[] arr) { //出自力扣熱題100
         return 0;
     }
 }
