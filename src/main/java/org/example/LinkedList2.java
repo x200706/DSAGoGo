44737ce0a359cbac87150bd32a6dd3eb3f21c450
Crystal <99391710+x200706@users.noreply.github.com>
2023-05-09 16:01:52 +0800

    update LinkedList.java file

--- src/main/java/org/example/LinkedList2.java
+++ src/main/java/org/example/LinkedList2.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 public class LinkedList2 {
     //234回文鏈表
     @Test
@@ -18,6 +20,32 @@ public class LinkedList2 {
     }
 
     public boolean isPalindrome(ListNode head) {
+        //是不是對稱的？
+        //Stack不是先進後出？可以運用
+        Stack<ListNode> stack = new Stack<>();
+
+        //遍歷鏈表，把它推入堆疊
+        ListNode current = head;
+        while (current != null) {
+            stack.push(current);
+            current = current.next;
+        }
+
+        //產生一個反過來的鏈表
+        head = current;
+        while (!stack.isEmpty()) {
+            current.next = stack.peek();
+            current = current.next;
+            stack.pop();
+        }
+        current.next = null;
+
+//        //看看兩個鏈表內容有無對稱
+//        while (current != null){
+//            head.next;
+//            current.next;
+//        }
+
         return false;
     }
 
