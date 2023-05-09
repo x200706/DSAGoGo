ddc6ffeae520dc077ca0ff1e8a933eb656766279
Crystal <99391710+x200706@users.noreply.github.com>
2023-05-09 14:32:28 +0800

    update LinkedList.java file

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -4,30 +4,38 @@ import org.junit.jupiter.api.Test;
 
 public class LinkedList {
     @Test
-    public void hasCycleTest(){
-
-    }
-    
-    public boolean hasCycle(ListNodeForHasCycle head) {
-        boolean flag = false;
-        //使用快慢指針，被倒追（相等）時flag變為true
-
-        //頭插法練習
-        head = new ListNodeForHasCycle(1);
+    public void hasCycleTest() {
+        ListNodeForHasCycle head = new ListNodeForHasCycle(3);
         ListNodeForHasCycle node2 = new ListNodeForHasCycle(2);
-        ListNodeForHasCycle node3 = new ListNodeForHasCycle(5);
-        ListNodeForHasCycle node4 = new ListNodeForHasCycle(3);
+        ListNodeForHasCycle node3 = new ListNodeForHasCycle(0);
+        ListNodeForHasCycle node4 = new ListNodeForHasCycle(-4);
 
         head.next = node2;
         node2.next = node3;
         node3.next = node4;
-        node4.next = head; //環形鏈表，尾巴將接續頭部
+        node4.next = node2;
+        System.out.println(hasCycle(head));
 
-        return flag;
+    }
+
+    public boolean hasCycle(ListNodeForHasCycle head) {
+        //使用快慢指針，被倒追（相等）時即返回true
+        //遍歷鏈表
+        ListNodeForHasCycle fast = head;
+        ListNodeForHasCycle slow = head;
+        while (fast != null && fast.next != null) { //這句是說，當fast本身不是null，fast.next也不是時，持續循環，也就是說 這是一個環狀鏈表才能成立
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Test
-    public void printTheLinkedList(){
+    public void printTheLinkedList() {
         //頭插法練習
         ListNodeForHasCycle head = new ListNodeForHasCycle(1);
         ListNodeForHasCycle node2 = new ListNodeForHasCycle(2);
@@ -41,14 +49,15 @@ public class LinkedList {
 
         //打印練習
         ListNodeForHasCycle current = head;
-        while (true){
+        while (true) {
             System.out.println(current.val);
-            if(current.next == head){
+            if (current.next == head) {
                 break;
             }
-            current=current.next;
+            current = current.next;
         }
     }
+
     class ListNodeForHasCycle {
         int val;
         ListNodeForHasCycle next;
