4173aa22a1b5c9058258c265f7f620bb3ae78b14
Crystal <chi200706@gmail.com>
2023-05-14 23:22:32 +0800

    add a junit test(QueueDS.java)

--- src/main/java/org/example/QueueDS.java
+++ src/main/java/org/example/QueueDS.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 public class QueueDS {
+    @Test
+    public void countStudentsTest(){
+        System.out.println(countStudents(new int[]{1, 1, 0, 0}, new int[]{0, 1, 0, 1}));
+    }
+
+    public int countStudents(int[] students, int[] sandwiches) {
+        return 0;
+    }
 }
