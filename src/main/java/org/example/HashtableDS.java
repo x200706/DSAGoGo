e907d3577901d3c3b2b37638ce06a432cdb0d307
Crystal <chi200706@gmail.com>
2023-05-16 22:28:39 +0800

    I will solve it tomorrow

--- src/main/java/org/example/HashtableDS.java
+++ src/main/java/org/example/HashtableDS.java
@@ -2,15 +2,32 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Hashtable;
 
 public class HashtableDS {
     @Test
-    public void twoSumTest(){
-        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3}, 4)));
+    public void twoSumTest() {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
+
     public int[] twoSum(int[] nums, int target) {
-        int[] output = {};
-        return output;
+        ArrayList<Integer> outputList = new ArrayList<>();
+        //宣告一張哈希表
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>(); //K表原傳入值，V表目標值減去傳入值後的數
+        //存值進哈希表
+        Arrays.stream(nums).forEach(n -> {
+            hashtable.put(n, target - n);
+        });
+        //迴圈
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (hashtable.get(nums[i]) == hashtable.get(nums[j])) {
+                    outputList.add(i);
+                }
+            }
+        }
+        return outputList.stream().mapToInt(i -> i).toArray();
     }
 }
