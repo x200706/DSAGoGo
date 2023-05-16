3ca079e30c67fc41dca61d5ad548b06040715814
Crystal <chi200706@gmail.com>
2023-05-16 21:57:48 +0800

    add a JunitTest for HashtableDS.java

---
+++ src/main/java/org/example/HashtableDS.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class HashtableDS {
+    @Test
+    public void twoSumTest(){
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3}, 4)));
+    }
+    public int[] twoSum(int[] nums, int target) {
+        int[] output = {};
+        return output;
+    }
+}
