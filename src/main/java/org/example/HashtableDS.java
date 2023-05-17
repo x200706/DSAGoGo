5b024ac98f8668fa2ca970e48352a0e09325b9e2
Crystal <chi200706@gmail.com>
2023-05-17 19:08:27 +0800

    I finish it

--- src/main/java/org/example/HashtableDS.java
+++ src/main/java/org/example/HashtableDS.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Hashtable;
 
 public class HashtableDS {
@@ -13,21 +14,17 @@ public class HashtableDS {
     }
 
     public int[] twoSum(int[] nums, int target) {
-        ArrayList<Integer> outputList = new ArrayList<>();
+        int[] ans = new int[2];
         //宣告一張哈希表
-        Hashtable<Integer, Integer> hashtable = new Hashtable<>(); //K表原傳入值，V表目標值減去傳入值後的數
-        //存值進哈希表
-        Arrays.stream(nums).forEach(n -> {
-            hashtable.put(n, target - n);
-        });
-        //迴圈
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>(); //K表傳入值，V表索引號
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (hashtable.get(nums[i]) == hashtable.get(target - nums[j])) { //要解決同樣的數進來的問題
-                    outputList.add(i);
-                }
+            if (hashtable.containsKey(target - nums[i])) {
+                ans[0] = hashtable.get(target - nums[i]);
+                ans[1] = i;
             }
+            hashtable.put(nums[i], i);
         }
-        return outputList.stream().mapToInt(i -> i).toArray();
+        return ans;
     }
 }
