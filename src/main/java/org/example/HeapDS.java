3acabad52adec29fb0686328c2ea568ec0cd7dce
Crystal <chi200706@gmail.com>
2023-05-20 13:58:43 +0800

    add a test

--- src/main/java/org/example/HeapDS.java
+++ src/main/java/org/example/HeapDS.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class HeapDS {
     @Test
     public void pickGiftsTest() {
-
+        System.out.println(pickGifts(new int[]{25, 64, 9, 4, 100}, 4));
     }
 
     public long pickGifts(int[] gifts, int k) {
