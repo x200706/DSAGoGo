e43b068a43334bdcf4f73083c366fd680c85f687
Crystal <chi200706@gmail.com>
2023-05-20 13:56:49 +0800

    add HeapDS.java file

---
+++ src/main/java/org/example/HeapDS.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class HeapDS {
+    @Test
+    public void pickGiftsTest() {
+
+    }
+
+    public long pickGifts(int[] gifts, int k) {
+        long ans = 0;
+        return ans;
+    }
+}
