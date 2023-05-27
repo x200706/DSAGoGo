6c7dbb884375cc7e3e2a1729d67122e3d676c3a5
Crystal <chi200706@gmail.com>
2023-05-27 22:20:04 +0800

    storage value in tree for test

--- src/main/java/org/example/TreeDS.java
+++ src/main/java/org/example/TreeDS.java
@@ -8,11 +8,31 @@ import java.util.List;
 public class TreeDS {
 
     @Test
-    public void test() {
+    public void inorderTraversalTest() {
+        //重點練習：二叉樹的資料儲存
+        //步驟一：先創造所有節點吧
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+
+        //步驟二：連接節點
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+
+        System.out.println(inorderTraversal(node1));
 
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
+        //小心陷阱！它會傳空的樹（此題邊際）
         return new ArrayList<>();
     }
 
