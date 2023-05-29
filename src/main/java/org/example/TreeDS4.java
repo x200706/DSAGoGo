74d3d7f813c2a06cab494932661bb2598f93fac6
Crystal <chi200706@gmail.com>
2023-05-29 12:31:59 +0800

    keep going

--- src/main/java/org/example/TreeDS4.java
+++ src/main/java/org/example/TreeDS4.java
@@ -15,18 +15,14 @@ public class TreeDS4 {
         TreeNode node1 = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
         TreeNode node3 = new TreeNode(2);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(5);
-        TreeNode node7 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(2);
+        TreeNode node6 = new TreeNode(2);
 
         //步驟二：連接節點
         node1.left = node2;
         node1.right = node3;
         node2.left = node4;
-        node2.right = node5;
         node3.left = node6;
-        node3.right = node7;
 
         System.out.println(isSymmetric(node1));
     }
@@ -39,10 +35,10 @@ public class TreeDS4 {
         inorderTraversal(invertTree(root), reverseTree);
 
         for (int i = 0; i < tree.size(); i++) {
-                if (tree.get(i) != reverseTree.get(i)) {
-                    flag = false;
-                    break;
-                }
+            if (tree.get(i) != reverseTree.get(i)) {
+                flag = false;
+                break;
+            }
         }
 
         return flag;
@@ -53,9 +49,11 @@ public class TreeDS4 {
         if (root == null) {
             return;
         }
+
         inorderTraversal(root.left, targetList);
         targetList.add(root.val);
         inorderTraversal(root.right, targetList);
+
     }
 
     //翻轉器
